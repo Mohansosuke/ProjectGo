2a2e5801c7cf0dab9f3b22a289d482ef832f7265
Mohansosuke <mohan.25.official@gmail.com>
2026-07-19 21:22:38 +0530

    Add email debug logs

--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -17,16 +17,11 @@ const sendEmail = async ({ email, subject, html }) => {
     console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
 
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
+      service: "gmail",
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
-      connectionTimeout: 30000,
-      greetingTimeout: 30000,
-      socketTimeout: 30000,
     });
 
     const mailOptions = {
