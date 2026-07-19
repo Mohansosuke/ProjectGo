83558b6a4dbe85f083181fa5c61ee722fa90e544
Mohansosuke <mohan.25.official@gmail.com>
2026-07-19 20:52:39 +0530

    Fix authentication cookie settings for production

--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -12,17 +12,17 @@ const sendEmail = async ({ email, subject, html }) => {
 
   try {
     const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-  connectionTimeout: 30000,
-  greetingTimeout: 30000, 
-  socketTimeout: 30000,
-});
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+      connectionTimeout: 30000,
+      greetingTimeout: 30000,
+      socketTimeout: 30000,
+    });
 
     const mailOptions = {
       from: `"ProjectGo" <${process.env.EMAIL_USER}>`,
